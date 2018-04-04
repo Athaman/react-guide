562806bc10d909de82661315afb20cf2e6538bbe
Keone Martin <keonemartin@gmail.com>
2018-04-04 12:20:18 +1000

    section 47: updating state immutably

--- src/App.js
+++ src/App.js
@@ -23,8 +23,9 @@ class App extends Component {
     });
   }
 
-  deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+  deletePersonHandler = (personIndex  ) => {
+    //  const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   }
